c6119be8bbe127b9f48c12589be87b99a7aa6440
lucatego <lucasalvites@gmail.com>
2026-08-02 01:26:31 -0500

    New: Implementación de los tipos de datos necesarios para Mesa. Se crea la conexión mesa-jugador como una interfaz para facilitar las pruebas.

--- internal/mesa/servidor.go
+++ internal/mesa/servidor.go
@@ -1,1 +1,14 @@
 package mesa
+
+import "github.com/amvz1704/pokerFight/internal/protocolo"
+
+// Es la interfaz que representa la conexión de un jugador con la mesa. Cualquier
+// tipo de conexión a implementar (websocket, TCP, de prueba, etc.) debe implementar
+// esta interfaz.
+// Esta interfaz es utilizada por la mesa para enviar mensajes a los jugadores y
+// recibir mensajes (acciones) de los jugadores.
+type ConexionMesaJugador interface {
+	EnviarMensaje(m protocolo.MensajeMesa, timeoutMs uint64) error // La mesa envía un mensaje al jugador. Debe tener un timeout si no recibe confirmación de la respuesta. En caso no reciba confirmación, se retira al jugador de la mesa.
+	SolicitarAccion(timeoutMs uint64) (protocolo.Accion, error)    // La mesa solicita una acción al jugador cuando sea su turno. Debe tener un timeout si no recibe confirmación de la respuesta. En caso no reciba confirmación, se opta por la acción más segura (check o fold) según el estado de la partida. En caso de error, se opta por la acción más segura (check o fold) según el estado de la partida.
+	Cerrar() error                                                 // Cerrar la conexión con el jugador. Se llama cuando se termina la partida o el jugador se retira voluntariamente.
+}
